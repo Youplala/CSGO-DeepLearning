c5ff361ca0ff37ce6cfef75bd69ae75127baae6a
Youplala <eliebrosset@gmail.com>
2021-02-06 23:12:38 +0100

    scrapping_works

---
+++ scrapping/hltvscraper/hltvscraper/spiders/HLTV.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class HLTVSpider(scrapy.Spider):
+    name = 'hltv'
+    start_urls = [
+        'https://www.hltv.org/results',
+        'https://www.hltv.org/results?offset=100',
+    ]
+    def parse(self, response):
+        yield {
+            'teams': response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "line-align", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "team", " " ))]/text()').getall(),
+            'score': response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "result-score", " " ))]//span/text()').getall(),
+        }
