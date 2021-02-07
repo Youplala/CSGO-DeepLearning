44a1b0213eb91d2da0ff7c0bcd0d7904a314f810
Dorian-rx <rouxdorian@eisti.eu>
2021-02-07 01:51:16 +0100

    Index.html added

--- flask/flask_index.py
+++ flask/flask_index.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
-app = Flask(_name)
+app = Flask(__name__)
 
-@app.route('/')
-def Hello():
-    message = "Hello World, My name is _____"
-    return message
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host  = '0.0.0.0', port = 8520)
+    app.run(host  = '0.0.0.0', port = 5100)
