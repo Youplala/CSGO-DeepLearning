037d3a990ac6b70b9c366c7192aa51a470ecca46
Youplala <eliebrosset@gmail.com>
2021-02-09 15:11:38 +0100

    add map names scrapping

--- scrapping/hltvscraper/hltvscraper/spiders/matches_info.py
+++ scrapping/hltvscraper/hltvscraper/spiders/matches_info.py
@@ -7,18 +7,28 @@ class MatchDetailSpider(scrapy.Spider):
 
     def parse(self, response):
         match_links = response.css('div.results-all div.result-con > a.a-reset')
-        yield from response.follow_all(match_links, self.parse_author)
+        yield from response.follow_all(match_links, self.parse_match)
 
-        pagination_links = response.css('a.pagination-next')
-        yield from response.follow_all(pagination_links, self.parse)
-
-    def parse_author(self, response):
+    def parse_match(self, response):
         def extract_with_css(query):
             return response.css(query).get(default='').strip()
-        
+
+        def get_maps():
+            maps = response.css('div.mapholder > div.played div.mapname::text').getall()
+            map_json = []
+            i=1
+            for map in maps:
+                map_json.append({
+                    'map'+ str(i) +'_name': map,
+                })
+                i+=1
+            return map_json
+
         yield {
             'team1': extract_with_css('div.team1-gradient div.teamName::text'),
             'team2': extract_with_css('div.team2-gradient div.teamName::text'),
-            'scores': response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "team", " " ))]//a+//div/text()').getall(),
+            'score1': extract_with_css('div.team1-gradient > div::text'),
+            'score2': extract_with_css('div.team2-gradient > div::text'),
             'date': extract_with_css('.timeAndEvent .date::text'),
+            'map': get_maps()
         }
