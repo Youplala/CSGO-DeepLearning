03af0204982e5e7604f4620d1c0965d58c7cf8c9
Youplala <eliebrosset@gmail.com>
2021-02-23 21:47:09 +0100

    clean predict code and add dropout layer

--- deeplearning/predict.py
+++ deeplearning/predict.py
@@ -1,3 +1,5 @@
+
+# Import all the necessary libraries
 import json
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,63 +13,115 @@ from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from sklearn.linear_model import LinearRegression
 
+# Read the csv file ignoring errors
 df = pd.read_csv('dataset/rounds.csv', error_bad_lines=False)
+
+
+# Show the dataset shape and types of columns
 df.shape
 df.dtypes
+
+
+# Investigate the empty values in the columns and fill them
+df.isnull().any()
+df = df.fillna(method='ffill')
+
+
+
+unique = df["map_score1"].unique()
+print(np.sort(unique)) # We see '-' so we replace it by 0
+df["map_score1"] = df["map_score1"].replace(['-'],'0').astype(str).astype(int)
+df["map_score2"] = df["map_score2"].replace(['-'],'0').astype(str).astype(int)
+
+
+# Extract the difference of scores into y
+y = df['map_score1'] - df['map_score2']
+
+
+# Select the useless columns and remove them
 useless_columns = [x+str(y) for x in ['team_player', 'player', 'kast'] for y in range(1,11)]
-useless_columns = ['id', 'score1', 'score2', 'map_number','team1', 'team2', 'date','map_name'] + useless_columns
-data = df.drop(useless_columns, axis=1)
-unique = data["map_score1"].unique()
-print(np.sort(unique))
-unique2 = data["map_score2"].unique()
-data["map_score1"] = data["map_score1"].replace(['-'],'0').astype(str).astype(int)
-data["map_score2"] = data["map_score2"].replace(['-'],'0').astype(str).astype(int)
-
-data.dtypes
-data.shape
-data.head(10)
-data.isnull().any()
-data = data.fillna(method='ffill')
-y = data['map_score1'] - data['map_score2']
-data = data.drop(['map_score1', 'map_score2'], axis=1)
-data.head()
-
-train_x, test_x, train_y, test_y = train_test_split(data, y, test_size = 0.3, random_state=7)
+useless_columns = ['id', 'score1', 'score2', 'map_score1', 'map_score2',
+'map_number','team1', 'team2', 'date','map_name'] + useless_columns
+x = df.drop(useless_columns, axis=1)
 
 
+# Preview final dataset
+x.dtypes
+print('New shape: ', x.shape)
+x.head(10)
+
+
+# Separate data into test and train
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size = 0.2, random_state=7)
+
+
+# Trying out a simple Linear Regression
 lr = LinearRegression().fit(train_x, train_y)
 y_pred = lr.predict(test_x)
 accuracy = lr.score(test_x,test_y)
-print(accuracy*100,'%')
+print('Linear Regression accuracy: ', accuracy*100,'%')
 
-#from sklearn.preprocessing import StandardScaler
-#sc = StandardScaler()
-#train_x = sc.fit_transform(train_x)
-#test_x = sc.transform(test_x)
 
+'''
+# Scale data
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+train_x = sc.fit_transform(train_x)
+test_x = sc.transform(test_x)
+'''
+
+# Instanciate Deep Learning Keras model
 model = keras.Sequential([
     keras.layers.Dense(200, input_dim=train_x.shape[1], kernel_initializer='normal', activation='relu'),
     keras.layers.Dense(150, kernel_initializer='normal', activation='relu'),
     keras.layers.Dense(100, kernel_initializer='normal', activation='relu'),
     keras.layers.Dense(50, kernel_initializer='normal', activation='relu'),
-    keras.layers.Dropout(0.5),
+    keras.layers.Dropout(0.5, noise_shape=None, seed=None),
     keras.layers.Dense(1),
 ])
-model.compile(loss='mse', optimizer='adam', metrics=['mae', 'mape'])
-history = model.fit(train_x,train_y,epochs=200,validation_split=0.2, batch_size = 64)
+
+
+# Parameters
+loss       = 'mse'
+optimizer  = 'adam'
+metrics    = ['mae', 'mape']
+epochs     = 150
+validation = 0.3
+batch      = 64
+
+
+# Compile and fit model
+model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+history = model.fit(train_x,train_y,epochs=epochs,validation_split=validation, batch_size = batch)
+
+
+# Predict and compute mean loss
 prediction = model.predict(test_x)
 rounded = np.rint(prediction)
 score = model.evaluate(test_x,test_y)
 print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
+
+
+# Compute difference between prediction true value and prediction
 a = np.subtract(np.array(pd.DataFrame(test_y, columns=['actual'])), np.array(pd.DataFrame(rounded, columns=['pred'])))
-sns.displot(a)
 
-len([x for x in a if np.abs(x)==0]) / len(a) * 100
+def accuracy(a, i):
+    acc = len([x for x in a if np.abs(x)<=i]) / len(a) * 100
+    return acc
 
-pred = pd.DataFrame(rounded, columns=['pred'])
-#df = pd.merge(pd.DataFrame(test_y, columns=['true']), pred)
+print('### Accuracy ###\n')
+for i in range(1,5):
+    print('Accuracy +-', i, 'rounds: ', accuracy(a, i))
 
-print(pred)
+
+# Plot predicition distribution
+preds = pd.DataFrame(rounded, columns=['preds'])
+sns.displot(preds)
+plt.title('Distribution of the predicted difference of rounds')
+plt.show()
+
+
+#Plot loss vs validation loss
 plt.title('Loss / Mean Squared Error')
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='test')
@@ -78,8 +132,3 @@ plt.show()
 # plt.title('Prediction')
 # plt.legend()
 # plt.show()
-
-
-sns.displot(pd.DataFrame(prediction))
-plt.title('Distribution of the predicted difference of rounds')
-plt.show()
